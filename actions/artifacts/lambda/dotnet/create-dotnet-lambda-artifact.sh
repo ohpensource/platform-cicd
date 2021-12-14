423aac5464f6764b90f9e271483316e3ea9a6d61
mmichna <mmichna@ohpen.com>
2021-12-14 21:47:24 +0100

    fix: GMP-393 - improve and fix some scripts and provide some output custom actions parameters

--- actions/artifacts/lambda/dotnet/create-dotnet-lambda-artifact.sh
+++ actions/artifacts/lambda/dotnet/create-dotnet-lambda-artifact.sh
@@ -20,9 +20,9 @@ assume_role() {
     CREDENTIALS_FILE_NAME="aws-credentials.json"
 	if [[ ! -f "$CREDENTIALS_FILE_NAME" ]]; then
 		AWS_ACCOUNT_ID=$1
-		ROLE_NAME=$2
+        ROLE_NAME=$2
 		ROLE_ARN="arn:aws:iam::$AWS_ACCOUNT_ID:role/$ROLE_NAME"
-		aws sts assume-role --role-arn $ROLE_ARN --role-session-name github-session > $CREDENTIALS_FILE_NAME
+    	aws sts assume-role --role-arn $ROLE_ARN --role-session-name github-session > $CREDENTIALS_FILE_NAME
 	fi
 
     export AWS_ACCESS_KEY_ID=$(jq -r '.Credentials.AccessKeyId' $CREDENTIALS_FILE_NAME)

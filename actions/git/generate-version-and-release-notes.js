f28189bf2708ac1bc6c2d3c1a24c4bd45bc29ec8
Martin Mitro <mmitro@ohpen.com>
2022-02-03 15:55:31 +0100

    feat: SPT-1 - generate only three number versions to adhere to semantic: MAJOR.MINOR.PATCH

--- actions/git/generate-version-and-release-notes.js
+++ actions/git/generate-version-and-release-notes.js
@@ -70,25 +70,42 @@ function getUpdatedVersion(version, changes) {
   let major = parseInt(versionFileContent[0], 10);
   let minor = parseInt(versionFileContent[1], 10);
   let patch = parseInt(versionFileContent[2], 10);
+  let secondary = 0;
+  if (versionFileContent.length > 3) {
+    secondary = parseInt(versionFileContent[3], 10);
+  }
 
   let newMajor = 0;
   let newMinor = 0;
   let newPatch = 0;
+  let newSecondary = 0;
   if (changes.some((change) => change.type === "break")) {
     newMajor = major + 1;
     newMinor = 0;
     newPatch = 0;
+    newSecondary = 0;
   } else if (changes.some((change) => change.type === "feat")) {
     newMajor = major;
     newMinor = minor + 1;
     newPatch = 0;
-  } else {
+    newSecondary = 0;
+  } else if ((changes.some((change) => change.type === "fix")) || versionFileContent.length === 3) {
     newMajor = major;
     newMinor = minor;
     newPatch = patch + 1;
+    newSecondary = 0;
+  } else {
+    newMajor = major;
+    newMinor = minor;
+    newPatch = patch;
+    newSecondary = secondary + 1;
   }
 
-  return `${newMajor}.${newMinor}.${newPatch}`;
+  if (versionFileContent.length === 3) {
+    return `${newMajor}.${newMinor}.${newPatch}`;
+  } else {
+    return `${newMajor}.${newMinor}.${newPatch}.${newSecondary}`;
+  }
 }
 function getChange(line) {
   if (line.startsWith(featPreffix)) {

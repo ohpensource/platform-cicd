424b17cb0e587dd490acb758b8f62d8d3f73f100
Damian Rebolo <drebolo@ohpen.com>
2022-02-21 14:16:43 +0100

    feat: SPT-209 constant file created with regex

---
+++ actions/git/constants.js
@@ -0,0 +1,4 @@
+convRegex =
+  /(?<type>feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert)(?<scope>\([a-z,]+\))?(?<breaking>!)?(?<colon>:{1})(?<space> {1})(?<subject>.*)/;
+
+module.exports = convRegex;
